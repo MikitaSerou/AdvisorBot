88281989ca8d08fe401cb748f48e55140f08dd52
MikitaSerou <Mikita.serou@gmail.com>
2021-05-16 13:52:02 +0300

    DB

---
+++ database/CITIESDB_PUBLIC_COUNTRY.sql
@@ -0,0 +1,5 @@
+insert into PUBLIC.COUNTRY (ID, CURRENCY, EMOJI_CODE, NAME)
+values  (1, 'BYN', ':by:', 'Беларусь'),
+        (2, 'UAH', ':ua:', 'Украина'),
+        (3, 'USD', ':us:', 'США'),
+        (4, 'RUB', ':ru:', 'Россия');
